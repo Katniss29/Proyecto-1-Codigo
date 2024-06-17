a8f0af4f68b693eea728a7fb5d14b358ab6f659b
Katniss29 <arceemi29@gmail.com>
2024-06-17 01:12:47 -0600

    Para el video 10 le agragamos @foreach y {{}} a los atributos de importancia

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,30 +1,26 @@
 <!doctype html> 
 
 <title>My Blog</title>
- <link rel="stylesheet" href="/app.css">
+
+<link rel="stylesheet" href="/app.css">
 
 
 <body>
 
-    <?php foreach ($posts as $post) : ?> 
-
+    @foreach ($posts as $post)
+       
         <article>
-
             <h1>
-                <a href="/posts/<?= $post->slug; ?>">
-                    <?= $post->title; ?>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a>
-            
             </h1>
 
             <div>
-                <?= $post->excerpt; ?>
+                {{ $post->excerpt }}
             </div>
-            
-
         </article>
-
-    <?php endforeach; ?>
+@endforeach
     
 </body>
 
