ee78a6495829eab44075b8abce565d50dcffe67c
Katniss29 <arceemi29@gmail.com>
2024-06-18 13:56:26 -0600

    Video 11 finalizado de databases, agregacion de nuevo codigo en Post, User, creacion de User automaticamente, de categorias automaticamente ,

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User; // Asegúrate de usar la ruta correcta para tu modelo User
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,50 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+
+
+
+        $user = User::factory()->create();
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family' 
+        ]);
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal' 
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work' 
+        ]);
+
+
+        Post::create([
+            'user_id' =>  $user->id,
+            'category_id' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => ' <p> Lorem ipsum dolar sit amet. </p>',
+            'body' => ' <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempore earum id! Repellendus, laudantium itaque. Corrupti, vel similique. Expedita ratione voluptatibus id repudiandae natus ab inventore cupiditate iusto maxime eveniet.</p>'
+
+        ]);
+
+        
+        Post::create([
+            'user_id' =>  $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Lorem ipsum dolar sit amet.',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi tempore earum id! Repellendus, laudantium itaque. Corrupti, vel similique. Expedita ratione voluptatibus id repudiandae natus ab inventore cupiditate iusto maxime eveniet.'
+
+        ]);
     }
 }
