2e11a3a518f77669c313675e47bb05f5245b1e27
Katniss29 <arceemi29@gmail.com>
2024-07-06 15:11:33 -0600

    Video 67 finalizado, se agraga funciones de modificar y eliminar post y rutas para un mejor funcionamiento

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\SessionsController;
 use Illuminate\Http\Request;
 use MailchimpMarketing\ApiClient;
 use App\Http\Controllers\NewsletterController;
+use App\Http\Controllers\AdminPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -40,10 +41,12 @@ Route::post('loginn', [SessionsController::class, 'store'])->middleware('guest')
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
-Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
-Route::post('admin/posts', [PostController::class, 'store'])->middleware('admin');
-
-
+Route::post('admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
+Route::get('admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
+Route::get('admin/posts', [AdminPostController::class, 'index'])->middleware('admin');
+Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
+Route::patch('admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
+Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy'])->middleware('admin');
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
